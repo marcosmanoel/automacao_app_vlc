5494a3a799bf47ad0fd092ef0d57455418b4f6a7
marcosmanoel <marcos.manoel1991@gmail.com>
2017-03-14 15:15:04 -0300

    Inserção e implementação do cucumber.yml para gerar reports em HTML

--- features/support/app_installation_hooks.rb
+++ features/support/app_installation_hooks.rb
@@ -6,6 +6,7 @@ end
 
 Before do |scenario|
   scenario = scenario.scenario_outline if scenario.respond_to?(:scenario_outline)
+  TEST_APP_PATH="C:/Users/marcos.alves/Desktop/7f3b85706265e718fd03609346f214b2_0.9.0.apk"
 
   feature = scenario.feature
   if FeatureMemory.feature != feature || ENV['RESET_BETWEEN_SCENARIOS'] == '1'
